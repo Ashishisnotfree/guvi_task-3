bd86fb9d6a068950a19fe7512545f308fa9f961d
AshishSuresh07 <107559577+Ashishisnotfree@users.noreply.github.com>
2022-06-21 20:15:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let myResume={
+    "basics": [
+      "name": "Ashish S",
+      "email": "ashishsuresh07@gmail.com",
+      "phone": 9003258694,
+      "degree": "B.E",
+      "location": {
+        "address": "Sobha Winchester Kovilambakkam",
+        "postalCode": 600117,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      ],
+    "education": [
+      {
+        "institution": "EASWARI ENGINEERING COLLEGE",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2017,
+        "batch end year": 2021,
+        "gpa": 7.6,
+      }
+    ],
+   
+    "languages": [
+      {
+        "language": "Tamil,English,Japanese",
+      }
+    ],
+    "interests": [
+      {
+        "name": "gaming,sports,",
+      }
+    ]
+  
+}
